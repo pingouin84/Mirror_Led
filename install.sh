79260ee72b8f4964fa326c85cad64af05c00fae2
Florian DUCORD <florian.ducord@gmail.com>
2019-06-01 13:51:08 -0700

    maj install

--- install.sh
+++ install.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
+sudo apt install python3 pythonpy python3-pip
 
-sudo  apt install python3.7
-sudo apt install pythonpy
-sudo apt install python3-pip
+pip3 install pygame
+pip3 install numpy
+pip3 install rpi_ws281x adafruit-circuitpython-neopixel
+pip3 install evdev
 
-#pip3 install pygame
-#pip3 install numpy
-#pip3 install rpi_ws281x adafruit-circuitpython-neopixel
-#pip3 install evdev
-
-python3.7 -m pip install numpy
-python3.7 -m pip install pygame
-python3.7 -m pip install rpi_ws281x adafruit-circuitpython-neopixel
-python3.7 -m pip install evdev
+#python3.7 -m pip install numpy
+#python3.7 -m pip install pygame
+#python3.7 -m pip install rpi_ws281x adafruit-circuitpython-neopixel
+#python3.7 -m pip install evdev
