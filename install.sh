9c4ed2f4261476e0f4edaa764f0e0670d249fc3c
pingouin84 <florian.ducord@gmail.com/usr/bin/python3.7>
2019-12-18 02:57:20 -0800

    modification install

--- install.sh
+++ install.sh
@@ -2,12 +2,4 @@
 
 sudo apt-get -y install python3 python3-dev python3-pip
 
-pip3 install pygame
-pip3 install numpy
-pip3 install rpi_ws281x adafruit-circuitpython-neopixel
-pip3 install evdev
-
-#python3.7 -m pip install numpy
-#python3.7 -m pip install pygame
-#python3.7 -m pip install rpi_ws281x adafruit-circuitpython-neopixel
-#python3.7 -m pip install evdev
+pip3 install -r requirements.txt
