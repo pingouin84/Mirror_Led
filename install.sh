40ff139950a4a72c0c3200aa44124bfaec683e8c
Florian DUCORD <florian.ducord@gmail.com>
2020-04-08 14:00:36 +0200

    modification script installation

--- install.sh
+++ install.sh
@@ -3,3 +3,5 @@
 sudo apt-get -y install python3 python3-dev python3-pip
 
 pip3 install -r requirements.txt
+
+cp ~/Mirror_Led/Mirror_led.desktop ~/.config/autostart/Mirror_led.desktop
--- run_test_dev.sh
+++ run_test_dev.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd $(dirname $0)
-python3 main.py
+python3 main.py debug
